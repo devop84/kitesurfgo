1485a5638a61e75bb61bafaceff6c62848696384
VALENTIN Francois <valentinpro@gmail.com>
2023-04-04 20:47:47 -0300

    Update slider.js

--- htmls/js/slider.js
+++ htmls/js/slider.js
@@ -1,5 +1,6 @@
 const slider = document.getElementById("slider");
 const slides = slider.querySelectorAll(".slide");
+
 let currentSlide = 0;
 let touchStartX = 0;
 let touchEndX = 0;
@@ -7,42 +8,38 @@ let touchEndX = 0;
 // Set the first slide to be active
 slides[currentSlide].classList.add("active");
 
-const progressBar = document.querySelector('.progress');
+const progressBar = document.querySelector(".progress");
 const progress = ((currentSlide + 1) / slides.length) * 100;
 progressBar.style.width = `${progress}%`;
 
+let intervalId = setInterval(() => {
+  changeSlide();
+}, 8000);
 
-
-// Create a timer to change the active slide every 8 seconds
-setInterval(() => {
-
-
+function changeSlide() {
   // Remove the active class from the current slide
   slides[currentSlide].classList.remove("active");
-  
+
   // Increment the current slide, or reset to 0 if at the end
   currentSlide = (currentSlide + 1) % slides.length;
-  
+
   // Set the next slide to be active
   slides[currentSlide].classList.add("active");
 
-  
-  const progressBar = document.querySelector('.progress');
+  const progressBar = document.querySelector(".progress");
   const progress = ((currentSlide + 1) / slides.length) * 100;
   progressBar.style.width = `${progress}%`;
-
-}, 8000);
-
-
+}
 
 // Add touch events to change slide when swiping
 slider.addEventListener("touchstart", (event) => {
   touchStartX = event.touches[0].clientX;
+  clearInterval(intervalId);
 });
 
 slider.addEventListener("touchend", (event) => {
   touchEndX = event.changedTouches[0].clientX;
-  
+
   // Determine the direction of the swipe
   if (touchStartX - touchEndX > 50) {
     // Swipe left, go to the next slide
@@ -50,21 +47,22 @@ slider.addEventListener("touchend", (event) => {
     currentSlide = (currentSlide + 1) % slides.length;
     slides[currentSlide].classList.add("active");
 
-    const progressBar = document.querySelector('.progress');
+    const progressBar = document.querySelector(".progress");
     const progress = ((currentSlide + 1) / slides.length) * 100;
     progressBar.style.width = `${progress}%`;
-
-
   } else if (touchEndX - touchStartX > 50) {
     // Swipe right, go to the previous slide
     slides[currentSlide].classList.remove("active");
     currentSlide = (currentSlide + slides.length - 1) % slides.length;
     slides[currentSlide].classList.add("active");
 
-    const progressBar = document.querySelector('.progress');
+    const progressBar = document.querySelector(".progress");
     const progress = ((currentSlide + 1) / slides.length) * 100;
     progressBar.style.width = `${progress}%`;
-
   }
-});
 
+  // Start a new interval after the slide has been changed
+  intervalId = setInterval(() => {
+    changeSlide();
+  }, 8000);
+});
