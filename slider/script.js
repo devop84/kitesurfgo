a221a83ffb6790348620942a3dd3366b4aaba44e
VALENTIN Francois <valentinpro@gmail.com>
2023-04-04 13:48:49 -0300

    test

--- slider/script.js
+++ slider/script.js
@@ -1,64 +1,61 @@
-// get our elements
+/*
+  This JS is from the following project:
+  https://github.com/bushblade/Full-Screen-Touch-Slider
+*/
+
 const slider = document.querySelector('.slider-container'),
   slides = Array.from(document.querySelectorAll('.slide'))
 
-// set up our state
 let isDragging = false,
   startPos = 0,
   currentTranslate = 0,
   prevTranslate = 0,
-  animationID,
+  animationID = 0,
   currentIndex = 0
 
-// add our event listeners
 slides.forEach((slide, index) => {
   const slideImage = slide.querySelector('img')
-  // disable default image drag
   slideImage.addEventListener('dragstart', (e) => e.preventDefault())
-  // pointer events
-  slide.addEventListener('pointerdown', pointerDown(index))
-  slide.addEventListener('pointerup', pointerUp)
-  slide.addEventListener('pointerleave', pointerUp)
-  slide.addEventListener('pointermove', pointerMove)
+
+  // Touch events
+  slide.addEventListener('touchstart', touchStart(index))
+  slide.addEventListener('touchend', touchEnd)
+  slide.addEventListener('touchmove', touchMove)
+
+  // Mouse events
+  slide.addEventListener('mousedown', touchStart(index))
+  slide.addEventListener('mouseup', touchEnd)
+  slide.addEventListener('mouseleave', touchEnd)
+  slide.addEventListener('mousemove', touchMove)
 })
 
-// make responsive to viewport changes
-window.addEventListener('resize', setPositionByIndex)
-
-// prevent menu popup on long press
+// Disable context menu
 window.oncontextmenu = function (event) {
   event.preventDefault()
   event.stopPropagation()
   return false
 }
 
-// use a HOF so we have index in a closure
-function pointerDown(index) {
+function touchStart(index) {
   return function (event) {
     currentIndex = index
-    startPos = event.clientX
+    startPos = getPositionX(event)
     isDragging = true
+
+    // https://css-tricks.com/using-requestanimationframe/
     animationID = requestAnimationFrame(animation)
     slider.classList.add('grabbing')
   }
 }
 
-function pointerMove(event) {
-  if (isDragging) {
-    const currentPosition = event.clientX
-    currentTranslate = prevTranslate + currentPosition - startPos
-  }
-}
-
-function pointerUp() {
-  cancelAnimationFrame(animationID)
+function touchEnd() {
   isDragging = false
+  cancelAnimationFrame(animationID)
+
   const movedBy = currentTranslate - prevTranslate
 
-  // if moved enough negative then snap to next slide if there is one
   if (movedBy < -100 && currentIndex < slides.length - 1) currentIndex += 1
 
-  // if moved enough positive then snap to previous slide if there is one
   if (movedBy > 100 && currentIndex > 0) currentIndex -= 1
 
   setPositionByIndex()
@@ -66,17 +63,28 @@ function pointerUp() {
   slider.classList.remove('grabbing')
 }
 
+function touchMove(event) {
+  if (isDragging) {
+    const currentPosition = getPositionX(event)
+    currentTranslate = prevTranslate + currentPosition - startPos
+  }
+}
+
+function getPositionX(event) {
+  return event.type.includes('mouse') ? event.pageX : event.touches[0].clientX
+}
+
 function animation() {
   setSliderPosition()
   if (isDragging) requestAnimationFrame(animation)
+}
+
+function setSliderPosition() {
+  slider.style.transform = `translateX(${currentTranslate}px)`
 }
 
 function setPositionByIndex() {
   currentTranslate = currentIndex * -window.innerWidth
   prevTranslate = currentTranslate
   setSliderPosition()
-}
-
-function setSliderPosition() {
-  slider.style.transform = `translateX(${currentTranslate}px)`
 }
