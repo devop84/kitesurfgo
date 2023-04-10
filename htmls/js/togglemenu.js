2cc49437fe0d9191bdab3420a47559a685b54a4e
VALENTIN Francois <valentinpro@gmail.com>
2023-04-10 17:34:16 -0300

    test

--- htmls/js/togglemenu.js
+++ htmls/js/togglemenu.js
@@ -1,6 +1,8 @@
 const menuToggle = document.getElementById('menu-toggle');
 const menu = document.getElementById('nav-list');
+const topbgchange = document.getElementById('top-bgchange');
 
 menuToggle.addEventListener('click', () => {
   menu.classList.toggle('show');
+  topbgchange.classList.toggle('showbg');
 });
